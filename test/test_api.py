4b7fdc692874d7da669cb0697566937f753b5451
y-chan <y_chan_crypto@yahoo.co.jp>
2020-09-25 12:31:35 +0900

    add register swap and get swap list test

--- test/test_api.py
+++ test/test_api.py
@@ -3,9 +3,11 @@ import tempfile
 import shutil
 
 from fastapi.testclient import TestClient
+from fastapi.encoders import jsonable_encoder
 from pycoin.encoding import b58
 
 from s4 import s4_api
+from s4.util import sha256d
 
 
 class TestAPI(unittest.TestCase):
@@ -57,3 +59,56 @@ class TestAPI(unittest.TestCase):
         self.assertTrue(isinstance(false_response_json, dict))
         self.assertTrue(isinstance(false_exist, bool))
         self.assertFalse(false_exist)
+
+    def test_register_swap_and_get_swap_list(self):
+        token_response = self.client.get("/get_token/")
+        token_response_json = token_response.json()
+        token = token_response_json.get("token")
+        self.assertEqual(token_response.status_code, 200)
+        self.assertTrue(isinstance(token_response_json, dict))
+        self.assertTrue(isinstance(token, str))
+        encodable = False
+        raw_token = ""
+        try:
+            raw_token = b58.a2b_base58(token)
+            encodable = bool(raw_token)
+        except Exception:
+            pass
+        self.assertTrue(encodable)
+
+        register_right_requests = {
+            "token": token,
+            "wantCurrency": "BTC",
+            "wantAmount": 10000,
+            "sendCurrency": "LTC",
+            "sendAmount": 100000000,
+            "receiveAddress": "12dRugNcdxK39288NjcDV4GX7rMsKCGn6B"
+        }
+
+        register_response = self.client.post("/register_swap/", json=jsonable_encoder(register_right_requests))
+        register_response_json = register_response.json()
+        status = register_response_json.get("status")
+        self.assertEqual(register_response.status_code, 200)
+        self.assertTrue(isinstance(register_response_json, dict))
+        self.assertEqual(status, "Success")
+
+        right_list_response = {
+            "initiatorCurrency": register_right_requests["wantCurrency"],
+            "initiatorReceiveAmount": register_right_requests["sendAmount"],
+            "participatorCurrency": register_right_requests["sendCurrency"],
+            "participatorReceiveAmount": register_right_requests["wantAmount"],
+            "participatorAddress": register_right_requests["receiveAddress"]
+        }
+        hashed_token_hex = sha256d(raw_token).hex()
+
+
+        list_response = self.client.get("/get_swap_list/")
+        list_response_json = list_response.json()
+        self.assertEqual(list_response.status_code, 200)
+        self.assertTrue(isinstance(list_response_json, dict))
+        data = list_response_json.get("data")
+        self.assertTrue(isinstance(data, dict))
+        exported_by_key = data.get(hashed_token_hex)
+        self.assertTrue(isinstance(exported_by_key, dict))
+        self.assertEqual(exported_by_key, right_list_response)
+
