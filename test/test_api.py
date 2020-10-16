d11127541f845036d33ff060d79dd6ab5dadeb4c
y-chan <y_chan_crypto@yahoo.co.jp>
2020-10-16 12:21:29 +0900

    [COMPLETE] refactor pytest

--- test/test_api.py
+++ test/test_api.py
@@ -9,7 +9,7 @@ from pycoin.encoding import b58
 from asns import asns_api
 from asns.util import sha256d
 
-from typing import Tuple
+from typing import Dict, Tuple
 
 
 class TestAPI(unittest.TestCase):
@@ -36,7 +36,7 @@ class TestAPI(unittest.TestCase):
         response_json = response.json()
         token = response_json.get("token")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(isinstance(response_json, dict))
+        self.assertTrue(isinstance(response_json, Dict))
         self.assertTrue(isinstance(token, str))
         encodable = False
         raw_token = b""
@@ -48,18 +48,33 @@ class TestAPI(unittest.TestCase):
         self.assertTrue(encodable)
         return token, raw_token
 
-    def verify_token(self, token: str, result: bool) -> None:
+    def verify_token(self, token: str, result: bool = True) -> None:
         response = self.client.post("/verify_token/", json={"token": token})
         response_json = response.json()
         exist = response_json.get("exist")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(isinstance(response_json, dict))
+        self.assertTrue(isinstance(response_json, Dict))
         self.assertTrue(isinstance(exist, bool))
         self.assertEqual(exist, result)
 
+    def register_swap(self, req_data: Dict, status_code: int = 200, result: str = "Success") -> None:
+        response = self.client.post("/register_swap/", json=jsonable_encoder(req_data))
+        response_json = response.json()
+        status = response_json.get("status")
+        self.assertEqual(response.status_code, status_code)
+        self.assertTrue(isinstance(response_json, Dict))
+        self.assertEqual(status, result)
+
+    def get_swap_list(self) -> Dict:
+        response = self.client.get("/get_swap_list/")
+        response_json = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(isinstance(response_json, Dict))
+        return response_json
+
     def test_get_token_and_verify_token(self):
         token, _ = self.get_token()
-        self.verify_token(token, True)
+        self.verify_token(token)
 
         false_token = "4esCzx3bbk2UNWLsxinLwGFfUv1zq5N5tUrirCMQWWBWkoxe5yrRYnkqWeqqViDodxSMT252Gif37c7UJp5RLPLy"
         self.verify_token(false_token, False)
@@ -76,12 +91,7 @@ class TestAPI(unittest.TestCase):
             "receiveAddress": "12dRugNcdxK39288NjcDV4GX7rMsKCGn6B"
         }
 
-        register_response = self.client.post("/register_swap/", json=jsonable_encoder(register_right_requests))
-        register_response_json = register_response.json()
-        status = register_response_json.get("status")
-        self.assertEqual(register_response.status_code, 200)
-        self.assertTrue(isinstance(register_response_json, dict))
-        self.assertEqual(status, "Success")
+        self.register_swap(register_right_requests)
 
         right_list_response = {
             "initiatorCurrency": register_right_requests["wantCurrency"],
@@ -92,13 +102,10 @@ class TestAPI(unittest.TestCase):
         }
         hashed_token_hex = sha256d(raw_token).hex()
 
-        list_response = self.client.get("/get_swap_list/")
-        list_response_json = list_response.json()
-        self.assertEqual(list_response.status_code, 200)
-        self.assertTrue(isinstance(list_response_json, dict))
+        list_response_json = self.get_swap_list()
         data = list_response_json.get("data")
-        self.assertTrue(isinstance(data, dict))
+        self.assertTrue(isinstance(data, Dict))
         exported_by_key = data.get(hashed_token_hex)
-        self.assertTrue(isinstance(exported_by_key, dict))
+        self.assertTrue(isinstance(exported_by_key, Dict))
         self.assertEqual(exported_by_key, right_list_response)
 
