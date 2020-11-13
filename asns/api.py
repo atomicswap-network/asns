eb6ffd5c043aa82419581791362e62075ab6aa82
y-chan <y-chan@y-chan.dev>
2020-11-14 03:25:32 +0900

    add data to initiator info api

--- asns/api.py
+++ asns/api.py
@@ -319,10 +319,14 @@ async def get_initiator_info(item: TokenItem, commons: DBCommons = Depends(db_co
 
         if SwapStatus.REGISTERED < (swap_data := commons.tx_db.get(hashed_token)).swap_status < SwapStatus.COMPLETED:
             initiator_address = swap_data.i_addr
+            initiator_contract = swap_data.i_contract
+            initiate_tx = swap_data.i_raw_tx
             token_hash = swap_data.i_token_hash
             result = {
                 "status": "Success",
                 "initiatorAddress": initiator_address,
+                "initiatorContract": initiator_contract,
+                "initiateRawTransaction": initiate_tx,
                 "tokenHash": token_hash.hex()
             }
         else:
@@ -330,6 +334,8 @@ async def get_initiator_info(item: TokenItem, commons: DBCommons = Depends(db_co
                 "status": "Failed",
                 "initiatorAddress": None,
                 "tokenHash": None,
+                "initiatorContract": None,
+                "initiateRawTransaction": None,
                 "error": "Swap has not initiated or has already completed."
             }
 
